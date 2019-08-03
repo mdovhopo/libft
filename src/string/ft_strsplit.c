4b1b46d7c71413da8597796d10e61ffba0203499
mdovhopo <mdovhopo@student.unit.ua>
2019-08-03 13:24:35 +0300

    remove logs

--- src/string/ft_strsplit.c
+++ src/string/ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: tryckylake <tryckylake@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/30 17:40:11 by mdovhopo          #+#    #+#             */
-/*   Updated: 2019/08/03 13:16:14 by tryckylake       ###   ########.fr       */
+/*   Updated: 2019/08/03 13:24:21 by tryckylake       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ char			**ft_strsplit(char const *s, char c)
 	int		words;
 
 	words = ft_count_words(s, c);
-	printf("Allocated memory of size %lu for %d words\n",(words + 1) * sizeof(char*), words + 1);
 	if ((split = malloc(sizeof(char *) * (words + 1))))
 	{
 		k = 0;
@@ -41,7 +40,6 @@ char			**ft_strsplit(char const *s, char c)
 			j = 0;
 			while (s[k] == c)
 				k++;
-			printf("Allocated memory of size %lu for word\n", sizeof(char) * (count_char(&s[k], c) + 1));
 			if ((split[i] = malloc(sizeof(char) * (count_char(&s[k], c) + 1))))
 			{
 				while (s[k] != c && s[k] != '\0')
